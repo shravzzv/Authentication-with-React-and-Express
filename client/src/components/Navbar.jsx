4392800b7857885b8a40f12c69b5837aa8257be5
sai shravan <saishravan384@gmail.com>
2024-06-20 09:03:53 +0530

    feat: Add navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='#'>logout</a>
+        </li>
+        <li>
+          <a href='/signin'>Sign In</a>
+        </li>
+        <li>
+          <a href='/signup'>Sign Up</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
