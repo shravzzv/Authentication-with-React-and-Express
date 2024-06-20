60c3d6260cb6eadb9142a6dc7d85e7799f831b4d
sai shravan <saishravan384@gmail.com>
2024-06-20 15:22:32 +0530

    feat(api): Send username and token as json on successful signup post route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,8 @@
+const { body, matchedData, validationResult } = require('express-validator')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken')
 
 /**
  * Renders the home page of the REST API.
@@ -10,10 +13,48 @@ exports.index = (req, res) => {
 
 /**
  * Handle the signup post request.
+ *
+ * Validates the username and password fields in the req.body and returns any errors if present.
+ * If no errors are present, creates a new user in the database and returns the user object with the username field and a jwt token as the response.
  */
 exports.signup = [
+  body('username')
+    .trim()
+    .isLength({ min: 3, max: 32 })
+    .withMessage('Username must be 3-32 characters long.')
+    .escape()
+    .custom(async (username) => {
+      const existingUser = await User.findOne({ username }, '_id')
+      if (existingUser) throw new Error(`Username ${username} already in use.`)
+    }),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be atleast 8 characters long.'),
+
   asyncHandler(async (req, res) => {
-    res.send('signup post not implemented')
+    const errors = validationResult(req)
+    const { username, password } = matchedData(req, { onlyValidData: false })
+
+    if (errors.isEmpty()) {
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+      })
+
+      await newUser.save()
+
+      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+      })
+
+      res.json({ username, token })
+    } else {
+      res.status(403).json(errors.array())
+    }
   }),
 ]
 
