07bb49e6322485a0b848d9b6a4e87b519617195c
sai shravan <saishravan384@gmail.com>
2024-06-21 10:22:49 +0530

    feat(client): Make a request for protected resource `users` from the About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,14 +1,54 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 export default function About() {
-  // todo: make an authenticated request to the api to get all users and display them here.
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true)
+        const token = JSON.parse(localStorage.getItem('token'))
+        const res = await axios.get('http://localhost:3000/users', {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        })
+        if (res.status === 200) setUsers(res.data)
+      } catch (error) {
+        setError(error.response.data.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return (
+      <>
+        <p>OOPS! An error occured.</p>
+        <p>{error}</p>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>About us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ab
-        repellat corporis, ratione, est accusamus amet magni officia blanditiis
-        quos laborum sunt accusantium, eligendi libero aliquid! Itaque
-        repellendus autem beatae!
-      </p>
+      <p>Members:</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>{user.username}</li>
+        ))}
+      </ul>
     </>
   )
 }
