72f67d12cccbbcf658a9adb1cc4998296eb856bd
sai shravan <saishravan384@gmail.com>
2024-06-21 08:12:00 +0530

    feat: Protect get all users route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var router = express.Router()
 const userController = require('../controllers/userController')
+const protect = require('../middleware/protect.middleware')
 
-router.get('/', userController.getUsers)
+router.get('/', protect, userController.getUsers)
 
 module.exports = router
